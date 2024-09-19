9034b1496e8bece37519d6d72e1733dfb9ab31f8
Ohad Maday <maday.ohad@gmail.com>
2024-09-19 17:08:33 +0300

    Added webpage

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');  // Make sure this line is present
+
+module.exports = {
+  entry: './src/index.js',  // Entry point for your app
+  output: {
+    path: path.resolve(__dirname, 'dist'),  // Output directory
+    filename: 'bundle.js',  // Name of the bundled file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,  // For JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],  // Babel preset for modern JavaScript
+          },
+        },
+      },
+      {
+        test: /\.css$/,  // For CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
